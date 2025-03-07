7324fb712c77fce04a3608d3e8be15b32087cd26
Yann-Michel Niquet <ymniquet@gmail.com>
2025-03-07 13:57:24 +0100

    Preparation of version 1.3.0.

--- src/equimagelab/equimage/image_colors.py
+++ src/equimagelab/equimage/image_colors.py
@@ -2,7 +2,7 @@
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 # Author: Yann-Michel Niquet (contact@ymniquet.fr).
-# Version: 1.2.0 / 2025.02.02
+# Version: 1.3.0 / 2025.03.07
 # Sphinx OK.
 
 """Color management."""
@@ -672,7 +672,7 @@ class MixinImage:
     Returns:
       Image: The processed image.
     """
-    self.check_color_model("lRGB", "sRGB")
+    self.check_color_space("lRGB", "sRGB")
     if hue == "red" or hue == "R":
       icc, ic1, ic2, negative = 0, 1, 2, False
     elif hue == "yellow" or hue == "Y":
